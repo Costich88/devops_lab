6b1f925e57c55211217fbe4299269bb6ca7378d4
Kanstantsin Novichuk <39298125+Costich88@users.noreply.github.com>
2018-07-12 19:17:36 +0300

    Update formattxt.py

--- formattxt.py
+++ formattxt.py
@@ -14,9 +14,9 @@ class FormTxt(fileout.FileOut):
                       (counter=dictVl['SNAPSHOT'], date=dictVl['DATE']),
                       'CPU load', 'Space Usage', 'RAM Usage', 'Bytes Read',
                       'Bytes Written', 'Traffic', sep="".ljust(10),
-                      file=open(self.filename+".txt", "a"))
+                      file=open(self.filename + ".txt", "a"))
 
                 print("".ljust(44), dictVl['CPU'], dictVl['Space Usage'],
                       dictVl['RAM Usage'], dictVl['Bytes Read'],
                       dictVl['Bytes Write'], dictVl['Traffic'],
-                      sep="".ljust(13), file=open(self.filename+".txt", "a"))
+                      sep="".ljust(13), file=open(self.filename + ".txt", "a"))
